014de59fd9e04e365712d572b09a183f5fcbaddc
gyandeepnaguri-crypto <gyandeepnaguri@gmail.com>
2026-01-29 22:29:45 -0500

    changes from main (#2)
    * All my changes  (#1)
    
    * Create Welcome.py
    
    * adding hello.py file
    
    ---------
    
    Co-authored-by: gyandeep17 <gyan.netdev@gmail.com>
    
    * commiting all 4 new files
    
    ---------
    
    Co-authored-by: gyandeep17 <gyan.netdev@gmail.com>

---
+++ Ifelse.py
@@ -0,0 +1,6 @@
+
+number = 3
+if number > 5:
+    print("the number is greator then 5")
+else:
+    print("the number is less than 5")
---
+++ switch.py
@@ -0,0 +1,12 @@
+day = 3
+match day:
+    case 1:
+       print("Monday")
+    case 2:
+        print("Tuesday")
+    case 3:
+        print("Wednesday")
+    case 4:
+        print("Thursday")
+    case _:
+        print("Invalid day")
