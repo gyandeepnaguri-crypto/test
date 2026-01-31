3f0d11c1c904ea7ee5e4da73262a9b4dc763bffc
gyandeep17 <gyan.netdev@gmail.com>
2026-01-30 21:24:58 -0500

    tuple and broswer

---
+++ tuple.py
@@ -0,0 +1,12 @@
+# create a tuple
+numbers = (10, 20, 30, 40)
+
+# print the tuple
+print(numbers)
+
+# access an element
+print("First element:", numbers[0])
+
+# loop through the tuple
+for n in numbers:
+    print(n)
